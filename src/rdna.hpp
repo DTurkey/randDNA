1cc79cd3614cc80283f2f1685433c89f2f6452cf
DTurkey <DTurkey@users.noreply.github.com>
2019-02-11 19:26:20 -0400

    Lab 2

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -5,14 +5,5 @@ using namespace std;
 
 string randDNA(int seed, string bases, int length)
 {
-	cout << " Please enter a number to seed the random number generator: " << endl;
-	cin >> seed;
-	
-	cout << " Please enter 'C' for cytosine, 'G' for guanine, 'A' for adenine or 'T' for thymine, : " << endl;
-	cin >>  bases;
-	
-	cout << " Please enter a number to indicate the length of sequence: " << endl;
-	cin >> length;
-	
 	return bases;
 }
