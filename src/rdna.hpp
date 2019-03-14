4d34a8f3c9749d8866d7a0e75c5db50650776f7d
DTurkey <DTurkey@users.noreply.github.com>
2019-03-13 20:31:04 -0400

    lab1 struggles

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 string randDNA(int seed, string bases, int n)
 {
-	for (unsigned int i=0; i < bases.size(); i++)
+	for ( int i = 0; i = n; i++)
 	{
-		cout << bases[i];
+		if (n > bases.size())
+			{
+				string dna;
+				dna = n + bases.size();
+				cout>>dna;
+			}
 	}
 return bases;
 }
