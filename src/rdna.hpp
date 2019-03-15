209b7be43b7288b1ddb5267c120ab92e0ba6d392
DTurkey <DTurkey@users.noreply.github.com>
2019-03-14 23:09:13 -0400

    Lab 2 progress

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,21 +1,22 @@
-#include <iostream>
 #include <string>
-#include <vector>
-
+#include <random>
 using namespace std;
 
-string randDNA(int seed, string bases, int n)
+string randDNA(int seed, string bases, int length)
 {
-	for (int i = 0; i < seed; i++)
-		for ( char ch: bases)
+	string base = "";
+	mt19937 seq1(seed);
+	uniform_int_distribution<int> dist(0, bases.size() -1);
+	
+	for (int i = 0; i < length; i++)
 		{
-			ch = rand()/n;
-			cout<<ch;
+			if (bases != "")
+				{
+					int random = dist(seq1);
+					base += bases[random];
+				}
 		}
-			
-			
-
-return " ";			
+return base;			
 }
 
 
